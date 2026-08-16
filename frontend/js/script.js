6960d7e7d11f515f041165d33dad6038184b960c
Akash Bajwa <bajwa6385@gmail.com>
2026-08-16 02:58:24 -0700

    Update contact form JavaScript

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -24,7 +24,7 @@ if (contactForm) {
         try {
 
             const response = await fetch(
-                "http://127.0.0.1:8000/contact",
+                "https://bajwa-dispatch-api.onrender.com/contact",
                 {
                     method: "POST",
 
